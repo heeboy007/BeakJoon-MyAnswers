3caa32a17f4c397522627ca235fcf2e352e89b9f
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-19 13:47:33 +0000

    update solution for 2630

--- BeakJoon/c++/solved/1~10/2000/2630.cpp
+++ BeakJoon/c++/solved/1~10/2000/2630.cpp
@@ -1,63 +1,66 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
-pair<int, int> devide_conquer(pair<int, int> check_cord, int n, vector<vector<int>> &paper){
-    int start_x = check_cord.first, start_y = check_cord.second;
-    bool check = (paper[start_x][start_y] == 1), all_valid = true;
-    pair<int, int> result = {0, 0};
-    for(int i = start_x; i < start_x + n; i++){
-        for(int j = start_y; j < start_y + n; j++){
-            if(check ^ paper[i][j]){
-                all_valid = false;
-                break;
-            }
-        }
-        if(!all_valid)
-            break;
-    }
+int blue = 0, white = 0;
+int map[150][150] = {0};
 
-    if(all_valid){
-        if(check){ //1이 second
-            result.second = 1;
-        } else { //0이 first
-            result.first = 1;
-        }
-    } else {
-        pair<int, int> dv_result;
-        vector<pair<int, int>> dv_try = {{start_x, start_y}, {start_x + n / 2, start_y},
-            {start_x, start_y + n / 2}, {start_x + n / 2, start_y + n / 2}};
-        for(pair<int, int> cord : dv_try){
-            dv_result = devide_conquer(cord, n / 2, paper);
-            result.first += dv_result.first;
-            result.second += dv_result.second;
-        }
-    }
-    return result;
+//device and conquer
+int dq(int n, int x, int y) {
+  if(n <= 1) {
+    return map[x][y];
+  }
+  int result[5] = {
+    dq(n / 2, x, y),
+    dq(n / 2, x + n / 2, y),
+    dq(n / 2, x, y + n / 2),
+    dq(n / 2, x + n / 2, y + n / 2)
+  };
+  /*cout << "N : " << n << " X : " << x << " Y : " << y << '\n';
+  cout << result[0] << '\n';
+  cout << result[1] << '\n';
+  cout << result[2] << '\n';
+  cout << result[3] << '\n';*/
+  if(result[0] == result[1] && result[1] == result[2] && result[2] == result[3]){
+    //cout << "Same!\n";
+    if(result[0] == 0)
+      return 0;
+    else if(result[0] == 1)
+      return 1;
+    else 
+      return 2;
+  } else {
+      for(int i = 0; i < 4; i++){
+        if(result[i] == 0)
+          white++;
+        else if(result[i] == 1)
+          blue++;
+      }
+      return 2;
+  }
+  
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n = 0;
+  cin >> n;
 
-    int n;
-    cin >> n;
-
-    vector<vector<int>> paper;
-
-    for(int i = 0; i < n; i++){
-        vector<int> slice;
-        for(int j = 0; j < n; j++){
-            int input;
-            cin >> input;
-            slice.emplace_back(input);
-        }
-        paper.push_back(std::move(slice));
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      cin >> map[i][j];
     }
-
-    pair<int, int> result = devide_conquer({0, 0}, n, paper);
-    cout << result.first << '\n' << result.second << '\n';
-
-    return 0;
+  }
+  
+  
+  int res = dq(n, 0, 0);
+  if(res == 0)
+    white++;
+  else if(res == 1)
+    blue++;
+  
+  cout << white << '\n' << blue;
+  return 0;
 }
